77e4f3099b596f29b5b7e63c049106b3503dead6
Shadab Ali <shadabali604@gmail.com>
2020-11-24 10:43:47 +0530

    added: completed

--- block-BNaabw/server.js
+++ block-BNaabw/server.js
@@ -1,13 +1,19 @@
 let express = require('express')
 let app = express();
+let path = require('path')
 app.use(express.static(__dirname + '/public'));
-
-
 app.get('/', (req, res) => {
-    console.log(__dirname + '/public');
-    res.sendFile(__dirname + '/index.html');
+     res.sendFile(__dirname + '/index.html');
 });
-
+app.get('/schedule', (req, res) => {
+    res.sendFile(__dirname + '/schedule.html')
+})
+app.get('/venue', (req, res) => {
+    res.sendFile(__dirname + '/venue.html')
+})
+app.get('/register', (req, res) => {
+    res.sendFile(__dirname + '/register.html')
+})
 app.listen('3000', (req, res) => {
     console.log('Server is running port 3k')
 })
